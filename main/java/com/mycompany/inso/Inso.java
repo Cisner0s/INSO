34c31c51fa67853af612a99917a9f5c74e076ca9
francarreram <91256788+francarreram@users.noreply.github.com>
2024-01-13 12:13:58 +0100

    Actualización interfaz

--- main/java/com/mycompany/inso/Inso.java
+++ main/java/com/mycompany/inso/Inso.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.inso;
-import com.mycompany.inso.IU.Principal;
+import com.mycompany.inso.IU.InicioSesion;
 import com.mycompany.inso.*;
 import com.mycompany.inso.BD.ControladorBD;
 import com.mycompany.inso.LOG.Director;
@@ -15,8 +15,8 @@ import com.mycompany.inso.LOG.Director;
 public class Inso {
 
     public static void main(String[] args) {
-        Principal prin = new Principal();
-        prin.setVisible(true);
+    	InicioSesion i = new InicioSesion();
+        i.setVisible(true);
         ControladorBD control = new ControladorBD();
         control.guardarDirector(new Director("nombre", "fecha_Nacimiento", "sexo", "lugar_nacimiento", "nacionalidad", "premios", "nominaciones"));
     }
